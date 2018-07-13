61c8c0df07044a886256b112fb3bb91007577581
Praveen Singh <praveen.singh@learningmatekol.local>
2018-07-13 17:43:47 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+       return (
+       <div>
+            <h1>Word Letter Focus 1</h1>
+            <h2>Word Letter Focus 2</h2>
+        </div>
+        );
+    }
+}
+
+export default App;
